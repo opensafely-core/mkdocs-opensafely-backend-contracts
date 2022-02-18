51773f9fb300a4b95a0294ae7096bc4d2c494b2b
George Hickman <george@ghickman.co.uk>
2022-02-18 12:13:02 +0000

    sort contracts by name

--- plugin/backends.py
+++ plugin/backends.py
@@ -16,7 +16,7 @@ def render_backend(data, match):
     if backend_data is None:
         raise UnknownClassException(f"Unknown class: {match}")
 
-    contracts = "\n".join(f"* `{c}`" for c in backend_data["tables"])
+    contracts = "\n".join(f"* `{c}`" for c in sorted(backend_data["tables"]))
 
     return backend_template.format(
         name=backend_data["name"],

7a1686a5f887e3b35d8dbe5fffd4bcfa066f0599
Jon Massey <jon.massey@thedatalab.org>
2022-06-09 15:08:11 +0100

    feat: render descriptive text in specs (#30)
    * feat: render descriptive text in specs
    
    * feat: update unit tests

--- plugin/specs.py
+++ plugin/specs.py
@@ -1,14 +1,14 @@
 chapter_template = """
-## {id} {title}
+## {id} {title}{text}
 """
 
 section_template = """
-### {id} {title}
+### {id} {title}{text}
 """
 
 
 paragraph_template = """
-#### {id} {title}
+#### {id} {title}{text}
 
 This example makes use of {intro} containing the following data:
 
@@ -56,10 +56,12 @@ def iter_input_tables(tables):
 def render_specs(specs_data):
     outputs = []
     for chapter in specs_data:  # 3
+        add_descriptive_text(chapter)
         output = chapter_template.format(**chapter)
         outputs.append(output)
 
         for section in chapter["sections"]:  # 3.1
+            add_descriptive_text(section)
             output = section_template.format(**section)
             outputs.append(output)
 
@@ -69,6 +71,7 @@ def render_specs(specs_data):
                 )
                 input_tables = "\n\n".join(iter_input_tables(paragraph["tables"]))
                 output_rows = "\n".join(build_rows(paragraph["output"]))
+                add_descriptive_text(paragraph)
 
                 output = paragraph_template.format(
                     **paragraph,
@@ -79,3 +82,7 @@ def render_specs(specs_data):
                 outputs.append(output)
 
     return "\n".join(outputs)
+
+
+def add_descriptive_text(data):
+    data["text"] = "\n" + data["text"] if "text" in data else ""
--- tests/plugin/test_main.py
+++ tests/plugin/test_main.py
@@ -20,7 +20,7 @@ def test_no_data_file(monkeypatch):
         DataBuilderPlugin().on_page_markdown("", None, None, None)
 
 
-def test_success(monkeypatch):
+def test_success_no_additional_text(monkeypatch):
     monkeypatch.setattr(plugin.main, "DATA_FILE", "tests/data.json")
 
     markdown = """
@@ -80,6 +80,94 @@ Contracts implemented:
 
 
 #### 1.1.1 Take with column
+
+This example makes use of an event-level table named `e` containing the following data:
+
+| |i1|b1 |
+| - | - | - |
+| 1|101|T |
+| 2|201|T |
+| 2|203|F |
+| 3|302|F |
+
+`e.take(e.b1).i1.sum_for_patient()` returns the following patient series:
+
+| patient | value |
+| - | - |
+| 1|203 |
+| 2|201 |
+| 3| |
+
+    """
+
+    assert output == expected
+
+
+def test_success_with_additional_text(monkeypatch):
+    monkeypatch.setattr(
+        plugin.main, "DATA_FILE", "tests/data_with_additional_text.json"
+    )
+
+    markdown = """
+# this is a page title
+
+!!! contracts
+
+!!! backend:DummyBackend1
+
+!!! specs
+    """
+
+    output = DataBuilderPlugin().on_page_markdown(markdown, None, None, None)
+
+    expected = """
+# this is a page title
+
+
+## Some/Path/DummyClass
+
+Dummy docstring
+
+| Column name | Description | Type | Constraints |
+| ----------- | ----------- | ---- | ----------- |
+| patient_id | a column description | PseudoPatientId | Must have a value, must be unique. |
+
+
+
+
+
+
+## Some/Path/DummyClass2
+
+Dummy docstring2.
+
+Second line.
+
+| Column name | Description | Type | Constraints |
+| ----------- | ----------- | ---- | ----------- |
+
+
+
+
+
+
+
+Contracts implemented:
+
+* [`Some/Path/DummyClass`](contracts-reference.md#somepathdummyclass)
+* [`Some/Path/DummyClass2`](contracts-reference.md#somepathdummyclass2)
+
+
+## 1 Filtering an event frame
+Chapters may have additional descriptive text blocks
+
+
+### 1.1 Including rows
+Additional text can also be added at a section level
+
+
+#### 1.1.1 Take with column
+Further additional text can be provided for a paragraph
 
 This example makes use of an event-level table named `e` containing the following data:
 
